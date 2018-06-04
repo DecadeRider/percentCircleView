7068c004946ec998390e2c51752aff183bf9377f
小小杰 <710454375@qq.com>
2018-06-04 10:53:10 +0800

    Initial Commit

---
+++ percentCircleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  percentCircleView
+//
+//  Created by 中投德利 on 2018/6/4.
+//  Copyright © 2018年 jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ percentCircleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  percentCircleView
+//
+//  Created by 中投德利 on 2018/6/4.
+//  Copyright © 2018年 jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
